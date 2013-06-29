682f3854fa138c0dbbdecebb62ae0eff9c2c812b
Isaac Rosenberg <irosenb7@gmail.com>
2013-06-29 19:32:12 -0400

    idk bro

--- data.js
+++ data.js
@@ -1,3 +1,19 @@
-function get_location() {
-  navigator.geolocation.getCurrentPosition(show_map);
+// function get_location() {
+//   navigator.geolocation.getCurrentPosition(show_map);
+// }
+
+// var x = document.getElementById("demo");
+
+// function show_map (position) {
+// 	x.innerHTML="Latitude: " + position.coords.latitude + 
+// 				"<br>Longitude: " + position.coords.longitude; 
+// }
+
+// get_location()
+
+function neighborhood (type) {
+	switch type	{
+		case "demographics":
+			
+	}
 }
